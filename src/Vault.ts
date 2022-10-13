c7a05372ce2f70dfb64d17da34665e26dcc665fb
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-10-13 23:50:22 +0900

    Added new types for vault.cryptomator file

--- src/Vault.ts
+++ src/Vault.ts
@@ -1,5 +1,33 @@
+import { DataProvider } from "./DataProvider";
+
+type VaultConfigHeader = {
+	kid: string;
+	typ: 'JWT';
+	alg: 'HS256' | 'HS384' | 'HS512';
+}
+
+type VaultConfig = {
+	format: number;
+	shorteningThreshold: number;
+	jti: string;
+	cipherCombo: 'SIV_CTRMAC';
+}
+
 export class Vault {
-	static async open(dir: string, password: string, name: string | null) {
-		
+	/**
+	 * Open an existing vault
+	 * @param provider Data provider
+	 * @param dir Directory of the vault that contains 'masterkey.cryptomator' and 'd' directory
+	 * @param password Password of the vault
+	 * @param name Name of the vault, may be null
+	 * 
+	 * Potential options later on:
+	 * Custom masterkey file
+	 * Custom vault.cryptomator file
+	 */
+	static async open(provider: DataProvider, dir: string, password: string, name: string | null) {
+		if (dir.endsWith('/')) dir += '/';
+		const jwt = provider.readFileString(dir + 'vault.cryptomator'); //The JWT is signed using the 512 bit raw masterkey
+
 	}
 }
