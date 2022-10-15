574cc56636bbb2b33debb0bf59da2ec6282fcce4
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-10-15 23:31:35 +0900

    Added tests against wrong passwords

--- test/decrypt.test.ts
+++ test/decrypt.test.ts
@@ -3,19 +3,21 @@ import path from "path";
 import { Vault } from '../src/Vault';
 import { LocalStorageProvider } from '../src/providers/LocalStorageProvider';
 
-async function decrypt(provider: LocalStorageProvider): Promise<Vault>{
-	const v = await Vault.open(provider, path.resolve(__dirname, 'Test'), 'qq11@@11', 'Test Vault');
+async function decrypt(provider: LocalStorageProvider, password: string): Promise<Vault>{
+	const v = await Vault.open(provider, path.resolve(__dirname, 'Test'), password, 'Test Vault');
 	return v;
 }
 
 describe('Test opening an existing vault', () => {
 	const provider = new LocalStorageProvider();
-	test('Check if LocalStorageProvider works', async () => {
-		expect(await provider.readFileString(path.resolve(__dirname, 'Test', 'qq11@@11.txt'))).toEqual('Hello world');
+	test('Wrong password should throw an error', async () => {
+		await expect(decrypt(provider, 'qq11@11')).rejects.toThrowError();
+		await expect(decrypt(provider, '')).rejects.toThrowError();
+		await expect(decrypt(provider, 'qq11@@@11')).rejects.toThrowError();
 	});
 	let vault: Vault;
-	test('Try opening a vault', async () => {
-		const pendingVault = decrypt(provider);
+	test('Try opening a vault with a correct password', async () => {
+		const pendingVault = decrypt(provider, 'qq11@@11');
 		await expect(pendingVault).resolves.not.toThrowError();
 		vault = await pendingVault;
 	});
