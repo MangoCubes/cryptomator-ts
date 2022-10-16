61f8db8c08c34e220563d0931e96c20c16fb39e6
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-10-16 19:18:02 +0900

    Added readFile property to DataProvider

--- src/DataProvider.ts
+++ src/DataProvider.ts
@@ -3,4 +3,5 @@ import { Item } from "./types";
 export type DataProvider = {
 	readFileString: (path: string) => Promise<string>;
 	listItems: (path: string) => Promise<Item[]>;
+	readFile: (path: string) => Promise<Uint8Array>;
 }
--- src/providers/LocalStorageProvider.ts
+++ src/providers/LocalStorageProvider.ts
@@ -3,6 +3,9 @@ import { Item } from "../types";
 import { promises as fs } from "fs";
 
 export class LocalStorageProvider implements DataProvider{
+	async readFile(path: string): Promise<Uint8Array>{
+		return new Uint8Array(await fs.readFile(path));
+	}
 	async readFileString (path: string): Promise<string>{
 		return await fs.readFile(path, 'utf-8');
 	}
