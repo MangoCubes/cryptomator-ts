1264dcd5f2e5931bd0e39dfdfd6e1cf4cfc2a99e
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-10-23 15:12:05 +0900

    Added test that encrypts and decrypts a simple file

--- test/encrypt.test.ts
+++ test/encrypt.test.ts
@@ -2,6 +2,8 @@ import { describe, expect, test } from '@jest/globals';
 import path from "path";
 import { Vault } from '../src/Vault';
 import { LocalStorageProvider } from '../src/providers/LocalStorageProvider';
+import { EncryptedFile } from '../src/encrypted/EncryptedFile';
+import { DirID } from '../src/types';
 
 describe('Test creating a vault', () => {
 	const provider = new LocalStorageProvider();
@@ -11,5 +13,19 @@ describe('Test creating a vault', () => {
 			name: 'Test2'
 		});
 		await expect(Vault.open(provider, path.resolve(__dirname, 'Test2'), '12341234', null)).resolves.not.toThrowError();
+	});
+	test.only('Try adding a file in root', async () => {
+		const dir = path.resolve(__dirname);
+		const v = await Vault.create(provider, dir, '12341234', {
+			name: 'Test3'
+		});
+		const testFunction = async () => {
+			await EncryptedFile.encrypt(v, 'HelloWorld.txt', '' as DirID, 'HELLO WORLD!');
+			const firstFile = (await v.listItems('' as DirID)).find(i => i.decryptedName === 'HelloWorld.txt') as EncryptedFile;
+			const decrypted = await firstFile.decryptAsString();
+			return decrypted.content.includes('HELLO WORLD!');
+		}
+		
+		await expect(testFunction()).resolves.toBe(true);
 	});
 });
