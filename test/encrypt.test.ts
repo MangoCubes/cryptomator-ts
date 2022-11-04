8c06c0e03744518a395c755facc0991eccb98816
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-11-04 23:20:46 +0900

    Added test to encrypt and decrypt

--- test/encrypt.test.ts
+++ test/encrypt.test.ts
@@ -5,6 +5,13 @@ import { LocalStorageProvider } from '../src/providers/LocalStorageProvider';
 import { EncryptedFile } from '../src/encrypted/EncryptedFile';
 import { DirID } from '../src/types';
 import { DecryptionError, DecryptionTarget } from '../src/Errors';
+import crypto from 'node:crypto';
+
+async function randomBuffer(size: number): Promise<Uint8Array>{
+	const arr = new Uint8Array(size);
+	for(let i = 0; i < size; i++) arr[i] = Math.floor(Math.random() * 256);
+	return arr;
+}
 
 describe('Test creating a vault', () => {
 	const provider = new LocalStorageProvider();
@@ -21,15 +28,30 @@ describe('Test creating a vault', () => {
 			name: 'encTest2'
 		});
 		const testFunction = async () => {
-			await EncryptedFile.encrypt(v, 'HelloWorld.txt', '' as DirID, 'HELLO WORLD!');
-			await EncryptedFile.encrypt(v, 'HelloWorld2.txt', '' as DirID, 'ABCD'.repeat(1024 * 1024) + '123412341234');
-			const firstFile = (await v.listItems('' as DirID)).find(i => i.decryptedName === 'HelloWorld.txt') as EncryptedFile;
-			const secondFile = (await v.listItems('' as DirID)).find(i => i.decryptedName === 'HelloWorld2.txt') as EncryptedFile;
-			const decrypted = await firstFile.decryptAsString();
-			const decrypted2 = await secondFile.decryptAsString();
-			return decrypted.content.includes('HELLO WORLD!') && decrypted2.content.includes('123412341234');
+			const limit = 20;
+			const buffers: Uint8Array[] = [];
+			const hashes: Uint8Array[] = [];
+			for(let i = 0; i < limit; i++) {
+				const b = await randomBuffer(Math.pow(2, i));
+				buffers.push(b);
+				hashes.push(crypto.createHash('sha256').update(b).digest());
+				await EncryptedFile.encrypt(v, `TestFile${i}.bin`, '' as DirID, b);
+			}
+			const items = await v.listItems('' as DirID);
+			for(let i = 0; i < limit; i++){
+				const item = items.find(it => it.decryptedName === `TestFile${i}.bin`);
+				if(!item || item.type === 'd') return false;
+				const fileContent = await item.decrypt();
+				const hash = crypto.createHash('sha256').update(fileContent.content).digest();
+				if(Buffer.compare(hash, hashes[i]) !== 0) return false;
+			}
+			return true;
 		}
 		
 		await expect(testFunction()).resolves.toBe(true);
+	});
+	afterAll(async () => {
+		const testVaults = await provider.listItems(dir);
+		for(const v of testVaults) await provider.removeDir(v.fullName);
 	});
 });
--- test/setup.ts
+++ test/setup.ts
@@ -1,8 +1,6 @@
 import { jest } from '@jest/globals';
-import webcrypto from 'crypto';
 import { TextEncoder } from 'util';
 
 global.TextEncoder = TextEncoder
-global.crypto = webcrypto.webcrypto as any
 
 jest.setTimeout(50000000);
